6506819ff76e1e9081967f4017a3822bf7c85587
Bernnardo Seraphim <bernnardosbo@gmail.com>
2023-11-13 14:35:36 -0300

    new repo from main repo

---
+++ api/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+#CORS(app, resources={r"/api/*": {"origins": ["http://localhost:30500", "http://127.0.0.1:80", "http://client:30500"]}})
+
+
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/api/recommend', methods=['POST'])
+def recommend():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            
+            if 'songs' not in data:
+                return jsonify({"error": "O campo 'songs' é obrigatório"}), 400
+
+            songs = data.get('songs')
+
+            if not songs:
+                return jsonify({"error": "A lista de 'songs' não pode estar vazia"}), 400
+            
+            if not isinstance(songs, list):
+                return jsonify({"error": "O campo 'songs' deve ser uma lista de strings"}), 400
+            
+            if not all(isinstance(song, str) for song in songs):
+                return jsonify({"error": "A lista de 'songs' deve conter apenas strings"}), 400
+            
+
+            # Chama o modelo de recomendação
+
+
+            response = {
+                "playlist_ids": [1, 2, 3],
+                "version": "1.0",
+                "model_date": "2023-10-12",
+            }
+
+            return make_response(jsonify(response), 200)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
+
+
+# by running 'flask run'     ->  __name__ != __main__
+# bu running 'python app.py' ->  __name__ == __main__
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=30530,
+        debug=True)
